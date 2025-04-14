c8777b9746b8de969e9b5cf58a618b445c47effa
kdyang <xzreder@gmail.com>
2025-04-13 20:12:02 -0400

    feat: Add heuristic head selection.

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,17 +2,18 @@
 
 HF_ENDPOINT=https://hf-mirror.com python scripts/download_model.py 
 
+# feature extraction
+
 python get_activations.py \
     Qwen1.5-14B-Chat \
     DRC \
     --model_dir "/root/autodl-tmp/models/Qwen1.5-14B-Chat" \
     --save_dir "/root/autodl-tmp/features/"
 
-# baseline
+# head selection
 
-config_name = "edited_model"
-
-# select heads by individual probing
+# option 1: select heads by individual probing
+config_name = "edited_model_baseline"
 python edit_weight.py \
     --model_name Qwen1.5-14B-Chat \
     --dataset_name DRC \
@@ -23,18 +24,8 @@ python edit_weight.py \
     --alpha 3 \
     --save_dir "/root/autodl-tmp/${config_name}/"
 
-# editing with fixed K
-python generate.py \
-    --model_path "/root/autodl-tmp/${config_name}/Qwen1.5-14B-Chat_dataset_DRC_seed_42_top_64_heads_alpha_3.0" \
-    --feature_path "/root/autodl-tmp/features/" \
-    --asset_path "/root/autodl-tmp/${config_name}/" \
-    --default_K 64
-
-# proposed
-
+# option 2: select heads by collaborative selection
 config_name = "edited_model_cs_l0_5_ln_25_svd_64_l1_0_gr_0.05"
-
-# select heads by collaborative selection
 python edit_weight.py \
     --model_name Qwen1.5-14B-Chat \
     --dataset_name DRC \
@@ -52,14 +43,29 @@ python edit_weight.py \
     --save_dir "/root/autodl-tmp/${config_name}/"
 # num_heads not used for collaborative selection
 
-# generate with fixed K
+# option 3: select heads by heuristic
+config_name = "edited_model_heuristic_64_3"
+python edit_weight.py \
+    --model_name Qwen1.5-14B-Chat \
+    --dataset_name DRC \
+    --activation_path "/root/autodl-tmp/features/Qwen1.5-14B-Chat_DRC_head_wise.npy" \
+    --label_path "/root/autodl-tmp/features/Qwen1.5-14B-Chat_DRC_labels.npy" \
+    --model_dir "/root/autodl-tmp/models/Qwen1.5-14B-Chat" \
+    --num_heads 64 \
+    --alpha 3 \
+    --selection_method heuristic \
+    --save_dir "/root/autodl-tmp/${config_name}/"
+
+# generation
+
+# option 1: editing with fixed K
 python generate.py \
     --model_path "/root/autodl-tmp/${config_name}/Qwen1.5-14B-Chat_dataset_DRC_seed_42_top_64_heads_alpha_3.0" \
     --feature_path "/root/autodl-tmp/features/" \
     --asset_path "/root/autodl-tmp/${config_name}/" \
     --default_K 64
 
-# generate with head-specific K
+# option 2: generate with head-specific K
 python generate.py \
     --model_path "/root/autodl-tmp/${config_name}/Qwen1.5-14B-Chat_dataset_DRC_seed_42_top_64_heads_alpha_3.0" \
     --feature_path "/root/autodl-tmp/features/" \
